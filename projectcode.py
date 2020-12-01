9592118ac1f3994c16dda0b0474a902583af70f8
Abhishek Rengarajan <abhir@berkeley.edu>
2020-11-30 19:35:17 -0800

    rudimentary lsi

--- projectcode.py
+++ projectcode.py
@@ -1,13 +1,19 @@
 import nltk
 import numpy as np
+from nltk.corpus import gutenberg
 from nltk.corpus import reuters
 import gensim
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
+from gensim.models import LsiModel
+from gensim.corpora import WikiCorpus
+from gensim import corpora
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
 
 # black boxed word2vec model from gensim
-def download_data():
+def download_data_as_text():
     for article in reuters.fileids():
         all_articles += reuters.raw(article)
 
@@ -29,6 +35,42 @@ def word_sim(word):
     else:
         print("this word is not in the corpus!")
 
-word_sim("economy")
+# word_sim("economy")
 
-def 
+def download_as_tokens():
+    all_articles=[]
+    for article in reuters.fileids():
+        all_articles.append(reuters.raw(article))
+    tokens = []
+    tokenizer = RegexpTokenizer(r'\w+')
+    en_stop = set(stopwords.words('english'))
+    for article in all_articles:
+        temp_token = tokenizer.tokenize(article.lower())
+        stopped_tokens = [i for i in temp_token if not i in en_stop]
+        tokens.append(stopped_tokens)
+    return tokens
+    #filename=open("test2.txt", "w")
+    #filename.write(all_articles)
+print("Got here")
+
+
+
+# Credit to https://www.datacamp.com/community/tutorials/discovering-hidden-topics-python for
+# helping figure out how to set up the document matrix
+def train_LSIModel(tokens):
+    # reuters_text = open("test2.txt", "r")
+    dct = corpora.Dictionary(tokens)
+    document_matrix = [dct.doc2bow(article) for article in tokens]
+    print("Got here")
+    model = LsiModel(document_matrix, num_topics=100, id2word=dct)
+    print("Got here")
+    model.save("test2.LSIModel")
+
+# outputs example topics
+def doc_sim():
+    model = gensim.models.LsiModel.load('test2.LSIModel')
+    print(model.print_topics(num_topics=20, num_words=10))
+
+tokens = download_as_tokens()
+train_LSIModel(tokens)
+doc_sim()
